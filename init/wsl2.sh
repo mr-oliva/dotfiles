51fdf10119c595ec9530c306ce72cb78eda9e6e5
bookun <kutsuzawa2851@gmail.com>
2019-09-17 01:56:05 +0900

    nvim

--- init/wsl2.sh
+++ init/wsl2.sh
@@ -1,6 +1,25 @@
-sudo apt update
+sudo apt update -y
 
 tmux -h
 if [ $? -ne 0 ]; then
-    sudo apt install tmux
+    sudo apt install tmux -y
 fi
+
+ructc -h
+if [ $? -ne 0 ]; then
+    curl https://sh.rustup.rs -sSf | sh
+fi
+
+nvim -h
+if [ $? -ne 0 ]; then
+    wget https://github.com/neovim/neovim/releases/download/v0.4.2/nvim-linux64.tar.gz
+    tar xvf nvim-linux64
+    nvim +PlugUpdate +qall > /dev/null
+fi
+
+go -h
+if [ $? -ne 0 ]; then
+    wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xvf go1.13.linux-amd64.tar.gz
+    GO111MODULE=on go get golang.org/x/tools/gopls@latest
+then
